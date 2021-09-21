5e997fc78c5cf05a6e2dff9d6eab2c5d565bb79b
Tom <tom@yata.fr>
2021-09-21 14:17:36 +0200

    first commit

---
+++ test-update.php
@@ -0,0 +1,14 @@
+<?php
+<?php
+/**
+ * @package Test Update
+ * @version 0.9
+ */
+/*
+Plugin Name: test Update
+Plugin URI: https://yata.fr/
+Description: Test
+Author: Tom
+Version: 0.9
+Author URI: http://ma.tt/
+*/
